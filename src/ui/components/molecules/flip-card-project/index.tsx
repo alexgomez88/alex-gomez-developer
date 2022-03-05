96d89ebb52cfcfc85d480e454a67802ac82ee840
Alejandro Gomez <alejandrogomez88@gmail.com>
2022-03-05 11:20:34 -0400

    update

--- src/ui/components/molecules/flip-card-project/index.tsx
+++ src/ui/components/molecules/flip-card-project/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { StructuredText } from "react-datocms";
 
 type FlipCardType = {
-    id: string;
-    title: string;
-    image: any;
-    description: any;
-    url: string;
-}
+  id: string;
+  title: string;
+  image: any;
+  description: any;
+  url: string;
+};
 
-export default function FlipCardProject({  title, image, description, url }: FlipCardType) {
+export default function FlipCardProject({
+  title,
+  image,
+  description,
+  url,
+}: FlipCardType) {
   return (
-      <div className="bg-sky-100 rounded-xl aspect-[4/3] overflow-hidden">
-
+    <div className="flip-card ">
+      <div className="flip-card-inner aspect-[4/3]">
+        <div className="flip-card-front "></div>
+        <div className="flip-card-back">
+          <StructuredText data={description} />
+        </div>
       </div>
+    </div>
   );
 }
